e8c16eeffc8630b88c04b7dc9066ebfe3a767f22
Gabriel Tache <takegabriel08@gmail.com>
2025-07-08 14:38:09 +0300

    display images correctly

--- resources/views/livewire/listing.blade.php
+++ resources/views/livewire/listing.blade.php
@@ -1,13 +1,22 @@
 <section class="bg-white py-8 antialiased dark:bg-gray-900 md:py-16">
     <div class="mx-auto max-w-screen-xl px-4 2xl:px-0">
         <div class="mx-auto max-w-5xl">
+            <a href="/" type="button"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <svg class="rotate-180 mr-2 w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M1 5h12m0 0L9 1m4 4L9 9" />
+                </svg>
+                Go back
+            </a>
             <h2 class="text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl">Job title: {{ $listing->title }}
             </h2>
             <h3 class="text-xl font-semibold text-gray-900 dark:text-white mt-4">Company: {{ $listing->company }}</h3>
             <div class="flex items-center justify-center my-8 xl:mb-16 xl:mt-12">
-                <img class="max-w-xl lg:max-w-full dark:hidden" src="{{ $listing->image ? asset('storage/' . $listing->logo) : asset('images/no-image.svg') }}" alt="" />
-                {{-- <img class="hidden max-w-xl lg:max-w-full dark:block"
-                    src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-showcase-dark.svg" alt="" /> --}}
+                <img class="max-w-xl lg:max-w-full"
+                    src="{{ $listing->logo ? asset('storage/' . $listing->logo) : asset('images/no-image.svg') }}"
+                    alt="" />
             </div>
             <div class="flex items-center justify-evenly my-6 md:my-12">
                 <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
@@ -20,7 +29,7 @@
                 <h4 class="text-xl font-semibold text-gray-900 dark:text-white">{{ $listing->location }}</h4>
             </div>
             <div class="flex items-center justify-evenly my-6 md:my-12">
-                <a href="mailto:{{$listing->email}}"
+                <a href="mailto:{{ $listing->email }}"
                     class="px-5 py-3 text-base font-medium text-center inline-flex items-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     <svg class="w-4 h-4 text-white me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                         fill="currentColor" viewBox="0 0 20 16">

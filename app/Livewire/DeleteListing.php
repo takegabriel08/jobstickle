a97ce505d44925326f8f3d65775ec3d00caaa314
Gabriel Tache <takegabriel08@gmail.com>
2025-07-08 23:58:27 +0300

    Only authorised users can delete listings

--- app/Livewire/DeleteListing.php
+++ app/Livewire/DeleteListing.php
@@ -4,16 +4,26 @@ namespace App\Livewire;
 
 use App\Models\Listing;
 use Livewire\Component;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class DeleteListing extends Component
 {
     public Listing $listing;
 
-    public function mount(Listing $listing){
+    public function mount(Listing $listing)
+    {
         $this->listing = $listing;
     }
-    public function deleteListing(Listing $listing){
-        $this->listing->delete();
+    public function deleteListing()
+    {
+        $this->authorize('delete', $this->listing);
+        return redirect()->to('/')->with('success', 'Listing deleted successfully!');
+
+        if ($this->listing->logo && Storage::disk('public')->exists($this->listing->logo)) {
+            Storage::disk('public')->delete($this->listing->logo);
+        }
+
         return redirect()->to('/')->with('success', 'Listing deleted successfully!');
     }
     public function render()

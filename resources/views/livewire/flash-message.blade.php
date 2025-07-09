6e3cbc2edab1ddbc3a76d5230f628041d0d33cea
Gabriel Tache <takegabriel08@gmail.com>
2025-07-09 10:19:54 +0300

    centered the flash message

--- resources/views/livewire/flash-message.blade.php
+++ resources/views/livewire/flash-message.blade.php
@@ -1,4 +1,4 @@
-<div id="someid" class="fixed top-6 left-1/2 transform-translate-x-1/2">
+<div id="someid" class="fixed top-6 left-1/2 -translate-x-1/2">
     @if (session()->has('success'))
         <div id="toast-default"
             class="flex items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow-sm dark:text-gray-400 dark:bg-gray-800"
